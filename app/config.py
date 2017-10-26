9550f38ed9d1475bc61697445b5d7740ece130d1
Sylvance <kerandisylvance@gmail.com>
2017-10-26 20:12:47 +0300

    [chore] Add configuration parameters

---
+++ app/config.py
@@ -0,0 +1,58 @@
+""" Configuration file"""
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+POSTGRES_LOCAL_BASE = 'postgresql://postgres:123456@localhost/'
+DATABASE_NAME = 'yummyrecipesdb'
+
+
+class BaseConfig:
+    """
+    Base application configuration
+    """
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_strong_key')
+    BCRYPT_HASH_PREFIX = 14
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AUTH_TOKEN_EXPIRY_DAYS = 30
+    AUTH_TOKEN_EXPIRY_SECONDS = 3000
+    BUCKET_AND_ITEMS_PER_PAGE = 25
+
+
+class DevelopmentConfig(BaseConfig):
+    """
+    Development application configuration
+    """
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', POSTGRES_LOCAL_BASE + DATABASE_NAME)
+    BCRYPT_HASH_PREFIX = 4
+    AUTH_TOKEN_EXPIRY_DAYS = 1
+    AUTH_TOKEN_EXPIRY_SECONDS = 20
+    BUCKET_AND_ITEMS_PER_PAGE = 4
+
+
+class TestingConfig(BaseConfig):
+    """
+    Testing application configuration
+    """
+    DEBUG = True
+    TESTING = True
+    POSTFIX = POSTGRES_LOCAL_BASE + DATABASE_NAME + "_test"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_TEST', POSTFIX)
+    BCRYPT_HASH_PREFIX = 4
+    AUTH_TOKEN_EXPIRY_DAYS = 0
+    AUTH_TOKEN_EXPIRY_SECONDS = 3
+    AUTH_TOKEN_EXPIRATION_TESTS = 5
+    BUCKET_AND_ITEMS_PER_PAGE = 3
+
+
+class ProductionConfig(BaseConfig):
+    """
+    Production application configuration
+    """
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', POSTGRES_LOCAL_BASE + DATABASE_NAME)
+    BCRYPT_HASH_PREFIX = 13
+    AUTH_TOKEN_EXPIRY_DAYS = 30
+    AUTH_TOKEN_EXPIRY_SECONDS = 20
+    BUCKET_AND_ITEMS_PER_PAGE = 10
