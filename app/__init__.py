7f1541e8a64893bb744d0c165f7fea45f0d0f2df
Sylvance <kerandisylvance@gmail.com>
2017-10-26 19:57:40 +0300

    [chore] Change initialisation code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,27 @@
 """ This main application initialisation """
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 APP = Flask(__name__)
 # APP.config.from_object('config')
+# Enabling cors
+CORS(APP)
+
+# APP configuration
+APP_SETTINGS = os.getenv(
+    'APP_SETTINGS',
+    'app.config.DevelopmentConfig'
+)
+APP.config.from_object(APP_SETTINGS)
+
+# Initialize BCRYPT
+BCRYPT = Bcrypt(APP)
+
+# Initialize Flask Sql Alchemy
+DB = SQLAlchemy(APP)
+
 
 from app import views
