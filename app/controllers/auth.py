2c6142acdd563942176df10ae0b7c6db681c698c
Sylvance <kerandisylvance@gmail.com>
2017-10-27 15:51:35 +0300

    [Delivers #152344827] Add controllers and helpers

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,25 +1,21 @@
-from app import bcrypt
+""" Authentication controller """
+from app import BCRYPT
 from flask import Blueprint, request
 from flask.views import MethodView
-from app.models import User, BlackListToken
-from app.auth.helper import response, response_auth
+from app.models import User, BannedToken
+from app.helpers.response_helper import response, response_auth
 from sqlalchemy import exc
 from app.helpers.auth_helper import token_required
 import re
 
-auth = Blueprint('auth', __name__)
+authentication = Blueprint('authentication', __name__)
 
 
 class RegisterUser(MethodView):
-    """
-    View function to register a user via the api
-    """
+    """ View function to register a user via the api """
 
     def post(self):
-        """
-        Register a user, generate their token and add them to the database
-        :return: Json Response with the user`s token
-        """
+        """ Register a user, generate their token and add them to the database :return: Json Response with the user`s token """
         if request.content_type == 'application/json':
             post_data = request.get_json()
             email = post_data.get('email')
@@ -36,53 +32,48 @@ class RegisterUser(MethodView):
 
 
 class LoginUser(MethodView):
+
     def post(self):
-        """
-        Login a user if the supplied credentials are correct.
-        :return: Http Json response
-        """
+        """ Login a user if the supplied credentials are correct. :return: Http Json response """
         if request.content_type == 'application/json':
             post_data = request.get_json()
             email = post_data.get('email')
             password = post_data.get('password')
             if re.match(r"[^@]+@[^@]+\.[^@]+", email) and len(password) > 4:
                 user = User.query.filter_by(email=email).first()
-                if user and bcrypt.check_password_hash(user.password, password):
-                    return response_auth('success', 'Successfully logged In', user.encode_auth_token(user.id), 200)
+                if user and BCRYPT.check_password_hash(user.password, password):
+                    return response_auth('success', 'Successfully logged In', user.encode_token(user.id), 200)
                 return response('failed', 'User does not exist or password is incorrect', 401)
             return response('failed', 'Missing or wrong email format or password is less than six characters', 401)
         return response('failed', 'Content-type must be json', 202)
 
 
 class LogOutUser(MethodView):
-    """
-    Class to log out a user
-    """
+    """ Class to log out a user """
 
     def post(self):
-        """
-        Try to logout a user using a token
-        :return:
-        """
+        """ Try to logout a user using a token :return: """
         auth_header = request.headers.get('Authorization')
         if auth_header:
             try:
-                auth_token = auth_header.split(" ")[1]
+                authentication_token = auth_header.split(" ")[1]
             except IndexError:
                 return response('failed', 'Provide a valid auth token', 403)
             else:
-                decoded_token_response = User.decode_auth_token(auth_token)
+                decoded_token_response = User.decode_token(
+                    authentication_token)
                 if not isinstance(decoded_token_response, str):
-                    token = BlackListToken(auth_token)
-                    token.blacklist()
+                    token = BannedToken(authentication_token)
+                    token.banned()
                     return response('success', 'Successfully logged out', 200)
                 return response('failed', decoded_token_response, 401)
         return response('failed', 'Provide an authorization header', 403)
 
 
-@auth.route('/authentication/reset/password', methods=['POST'])
+@authentication.route('/authentication/reset/password', methods=['POST'])
 @token_required
 def reset_password(current_user):
+    """ Reset the password"""
     if request.content_type == "application/json":
         data = request.get_json()
         old_password = data.get('oldPassword')
@@ -90,7 +81,7 @@ def reset_password(current_user):
         password_confirmation = data.get('passwordConfirmation')
         if not old_password or not new_password or not password_confirmation:
             return response('failed', "Missing required attributes", 400)
-        if bcrypt.check_password_hash(current_user.password, old_password):
+        if BCRYPT.check_password_hash(current_user.password, old_password):
             if not new_password == password_confirmation:
                 return response('failed', 'New password does not match', 400)
             if not len(new_password) > 4:
@@ -107,6 +98,9 @@ login_view = LoginUser.as_view('login')
 logout_view = LogOutUser.as_view('logout')
 
 # Add rules for the api Endpoints
-auth.add_url_rule('/authentication/register', view_func=registration_view, methods=['POST'])
-auth.add_url_rule('/authentication/login', view_func=login_view, methods=['POST'])
-auth.add_url_rule('/authentication/logout', view_func=logout_view, methods=['POST'])
+authentication.add_url_rule('/authentication/register',
+                  view_func=registration_view, methods=['POST'])
+authentication.add_url_rule('/authentication/login',
+                  view_func=login_view, methods=['POST'])
+authentication.add_url_rule('/authentication/logout',
+                  view_func=logout_view, methods=['POST'])
