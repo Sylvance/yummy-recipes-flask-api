8152ce7a7280d8d5c49f0203f447bc32fc05a5ad
Sylvance <kerandisylvance@gmail.com>
2017-10-27 15:39:37 +0300

    [Finishes #152344827] Add controllers and helpers

--- app/views.py
+++ app/views.py
@@ -1,12 +1,31 @@
 """ Views file for the Flask APP"""
-
 from app import APP
+from app.helpers.response_helper import response
 
 @APP.route('/')
 @APP.route('/index')
 def index():
     """ Return statement """
     return "Hello Yummy recipes"
+
+
+@APP.errorhandler(404)
+def route_not_found(e):
+    """ Return a custom 404 Http response message for missing or not found routes. :param e: Exception :return: Http Response """
+    return response('failed', 'Endpoint not found', 404)
+
+
+@APP.errorhandler(405)
+def method_not_found(e):
+    """ Custom response for methods not allowed for the requested URLs :param e: Exception :return: """
+    return response('failed', 'The method is not allowed for the requested URL', 405)
+
+
+@APP.errorhandler(500)
+def internal_server_error(e):
+    """ Return a custom message for a 500 internal error :param e: Exception :return: """
+    return response('failed', 'Internal server error', 500)
+
 
 # set the secret key.  keep this really secret:
 APP.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
